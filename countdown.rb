253beb218989721da031068e708578732dc1a5e9
Tra To <tothutra@gmail.com>
2018-08-12 03:23:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  n = seconds
+  while n>0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  n = seconds
+  while n>0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
